b26f4ce50b69e203a353b33ad41e097d74ce30c7
samurai01 <samurai01.kgsh@gmail.com>
2024-07-25 11:49:45 -0300

    test: erro ao nao encontrar

--- test/KendoLondrina.EndToEndTests/Api/Graduacao/DeleteGraduacaoTest.cs
+++ test/KendoLondrina.EndToEndTests/Api/Graduacao/DeleteGraduacaoTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KenLo.EndToEndTests.Api.Graduacao;
 
@@ -12,6 +13,8 @@ public class DeleteGraduacaoTest : IDisposable
 
     public DeleteGraduacaoTest(DeleteGraduacaoFixture fixture) 
         => _fixture = fixture;
+    public void Dispose()
+        => _fixture.CleanPersistence();
 
     [Fact(DisplayName = nameof(DeleteGraduacao))]
     public async void DeleteGraduacao()
@@ -33,27 +36,25 @@ public class DeleteGraduacaoTest : IDisposable
         persistenceGraduacao.Should().BeNull();
     }
 
-    // [Fact(DisplayName = nameof(ErrorWhenNotFound))]
-    // public async void ErrorWhenNotFound()
-    // {
-    //     var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
-    //     await _fixture.Persistence.InsertList(exampleCategoriesList);
-    //     var randomGuid = Guid.NewGuid();
+    [Fact(DisplayName = nameof(ErrorWhenNotFound))]
+    public async void ErrorWhenNotFound()
+    {
+        var graduacoes = _fixture.GetGraduacoes(20);
+        await _fixture.Persistence.InsertList(graduacoes);
+        var randomGuid = Guid.NewGuid();
 
-    //     var (response, output) = await _fixture.ApiClient.Delete<ProblemDetails>(
-    //         $"/categories/{randomGuid}"
-    //     );
+        var (response, output) = await _fixture.ApiClient.Delete<ProblemDetails>(
+            $"/graduacoes/{randomGuid}"
+        );
 
-    //     response.Should().NotBeNull();
-    //     response!.StatusCode.Should()
-    //         .Be((HttpStatusCode)StatusCodes.Status404NotFound);
-    //     output.Should().NotBeNull();
-    //     output!.Title.Should().Be("Not Found");
-    //     output!.Type.Should().Be("NotFound");
-    //     output!.Status.Should().Be((int)StatusCodes.Status404NotFound);
-    //     output!.Detail.Should().Be($"Category '{randomGuid}' not found.");
-    // }
-    public void Dispose()
-        => _fixture.CleanPersistence();
+        response.Should().NotBeNull();
+        response!.StatusCode.Should()
+            .Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Title.Should().Be("Not Found");
+        output!.Type.Should().Be("NotFound");
+        output!.Status.Should().Be((int)StatusCodes.Status404NotFound);
+        output!.Detail.Should().Be($"Graduacao {randomGuid} not found");
+    }
 
 }
